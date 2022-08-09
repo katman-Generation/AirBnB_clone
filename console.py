4f69a9df39c43c71fa9c53c99d48e880c36205d3
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-08-09 13:55:43 +0200

    Update console.py

--- console.py
+++ console.py
@@ -9,23 +9,31 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    def do_EOF(self, inp):
-        '''
-        Exit program with ctr-D
-        '''
-        return True
+   def do_EOF(self, line):
+       """
+        Exit the program on keyboard shortcut Ctr-D
+        :param line:
+        :return:
+        """
+       print()
+       return True
 
-    def do_quit(self, inp):
-        '''
-        Exit program with quit
-        '''
+    def do_quit(self, line):
+        """
+        Exit the program when you type keyword quit
+        :param line:
+        :return:
+        """
         return True
 
     def emptyline(self):
-        '''
-        Emptyline + enter should execute nothing
-        '''
+        """
+        An requested, an empty line + ENTER shouldnt execute anything.
+        So it will overwrite default behaviour to repeat the most recent command
+        :return:
+        """
         pass
+
 
 
 if __name__ == "__main__":
